47603d0bab304e28df7d036a5ccf1b42cd0ef908
Ivanov Anton <3ton.ua@gmail.com>
2016-11-24 20:31:28 +0200

    1st

---
+++ app.js
@@ -0,0 +1,39 @@
+var app = angular.module('youtube', []);
+
+app.controller('VideosController', function () {
+    this.tab = 1;
+    this.selectTab = function (tab) {
+        this.tab = tab
+    };
+    this.videos = [{
+        image: "https://docs.angularjs.org/img/angularjs-for-header-only.svg",
+        title: "Hi1",
+        e: true,
+        views: 100025,
+        description: "adsf fgnjjfv vjfdvj",
+        isFavorite: true
+    },{
+        image: "https://docs.angularjs.org/img/angularjs-for-header-only.svg",
+        title: "Hi2",
+        e: false,
+        views: 1044550,
+        description: "adsf fgnjj bhv fbfhfv vjfdvj",
+        isFavorite: false
+    },{
+        image: "https://docs.angularjs.org/img/angularjs-for-header-only.svg",
+        title: "Hi3",
+        e: false,
+        views: 1534267300,
+        description: "adsf fgnjjfv ff d f dfejfjf  f df vjfdvj",
+        isFavorite: true
+    }];
+
+    this.favorites = [{
+        image: "https://docs.angularjs.org/img/angularjs-for-header-only.svg",
+        title: "Hi4",
+        e: true,
+        views: 100024575545,
+        description: "adsf fgnjjfv vjfdvj",
+        isFavorite: true
+    }]
+});
